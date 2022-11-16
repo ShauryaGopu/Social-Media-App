f2c7d29718fb1facc1e2c90a1cd5d19f1467dd40
ShauryaGopu <gopal525shaurya@gmail.com>
2022-11-17 00:57:04 +0530

    Added all the template and static files.

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(STATIC_URL, 'staticfiles')
+STATICFILES_DIRS = [(BASE_DIR / 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
