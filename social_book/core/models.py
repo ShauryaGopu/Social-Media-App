9e865f0896a0f9b7c93db7b0dbc32833d31ae620
ShauryaGopu <gopal525shaurya@gmail.com>
2022-11-18 01:43:28 +0530

    Successfully completed the Profile Model, done Migrations

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to = 'profile_images', default = 'blank-profile-picture.jpg')
+    location = models.CharField(max_length = 255, blank = True)
+
+
+    def __str__(self):
+        return self.user.username
--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -125,3 +125,8 @@ STATICFILES_DIRS = [(BASE_DIR / 'static'),]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = (BASE_DIR / 'media')
