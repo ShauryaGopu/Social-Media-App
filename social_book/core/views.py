df4a956b6e9941a7a19b80bbbbd2d75b2812c276
ShauryaGopu <gopal525shaurya@gmail.com>
2022-11-28 18:08:09 +0530

    Completed the urls module

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -7,3 +7,13 @@ def index(request):
     return render(request, 'index.html')
 
 # Create your views here.
+
+def signup(request):
+
+    if request.method == 'POST':
+        username = request.POST('username')
+        email = request.POST('email')
+        password = request.POST('password')
+        print(username, email, password)
+    else:
+        return render(request, 'signup.html')
