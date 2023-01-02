afd952a96939d79435c0d930e991aa4a4341fdb5
ShauryaGopu <gopal525shaurya@gmail.com>
2023-01-02 11:01:24 +0530

    This is the new commit for second version.

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('settings',views.settings,name = 'settings'),
     path('settings',views.settings,name = 'settings'),
     path('upload',views.upload,name = 'upload'),
+     path('like-post',views.like_post,name = 'like-post'),
 
 ]
